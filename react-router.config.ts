4a81e47d3097c0ad516daa875348bebf05430fa8
Luka <luka.vukovic511@gmail.com>
2026-03-18 02:06:18 +0100

    final version

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,7 +4,6 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false` 
   ssr: false,
-  basename: "/djvrana-web/",
   prerender: true, 
   // async prerender() {
   //   return [

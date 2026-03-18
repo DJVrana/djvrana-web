0572350ef9667cc092424490198ca6048abcd742
Luka <luka.vukovic511@gmail.com>
2026-03-18 01:36:00 +0100

    final version

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,22 +4,22 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false` 
   ssr: false,
-  basename: "/djvrana-web",
-  async prerender() {
-    return [
-      "/",
-      "/o-meni",
-      "/usluge",
-      "/galerija",
-      "/kontakt",
-      "/vjencanja",
-      "/korporativni-dogadaji",
-      "/privatne-proslave",
-      "/dj-edukacija",
-      "/najam-opreme",
-      "/politika-privatnosti",
-      "/uvjeti-koristenja",
-      "/404",
-    ];
-  },
+  prerender: true, 
+  // async prerender() {
+  //   return [
+  //     "/",
+  //     "/o-meni",
+  //     "/usluge",
+  //     "/galerija",
+  //     "/kontakt",
+  //     "/vjencanja",
+  //     "/korporativni-dogadaji",
+  //     "/privatne-proslave",
+  //     "/dj-edukacija",
+  //     "/najam-opreme",
+  //     "/politika-privatnosti",
+  //     "/uvjeti-koristenja",
+  //     "/404",
+  //   ];
+  // },
 } satisfies Config;

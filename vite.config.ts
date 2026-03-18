d98f72328e2ae33c85c814b82463f83a045433dc
Luka <luka.vukovic511@gmail.com>
2026-03-18 03:45:58 +0100

    final version

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   base: "/djvrana-web/",
-  build: {
-    outDir: 'build/client',  // tvoj trenutni
-  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });

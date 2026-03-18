e0a182bab0854a77ad9da20dabe058d7935b7b84
Luka <luka.vukovic511@gmail.com>
2026-03-18 03:11:20 +0100

    final version

--- app/root.tsx
+++ app/root.tsx
@@ -43,7 +43,7 @@ export const links: Route.LinksFunction = () => [
   { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
   { rel: "shortcut icon", href: "/favicon.ico" },
   { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
-  { rel: "manifest", href: "/site.webmanifest" },
+  { rel: "manifest", href: "/djvrana-web/site.webmanifest" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
